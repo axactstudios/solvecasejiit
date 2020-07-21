6e6e2f826dbd8a5916608e4cc6b4d9c37ff23544
dranzerAvi <aviralhtc2015@gmail.com>
2020-07-22 02:53:12 +0530

    Bottom Navbar Added🤘

---
+++ lib/Screens/StudyMaterial.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class StudyMaterial extends StatefulWidget {
+  @override
+  _StudyMaterialState createState() => _StudyMaterialState();
+}
+
+class _StudyMaterialState extends State<StudyMaterial> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF33312E),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
